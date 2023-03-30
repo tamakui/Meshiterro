a4d9d0eefa7205dc4ea249f2644f76a21199a166
tamakui <kobjiuerw257jg@gmail.com>
2023-03-30 13:23:31 +0000

    7章deviseを使用した実装(3)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "06acf4845a5a4222afd4cc9c625393fa.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
