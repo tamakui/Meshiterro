2147b03e902c0e4269fa537a7495a035852c4c79
tamakui <kobjiuerw257jg@gmail.com>
2023-03-30 18:15:30 +0000

    [Finish]17章

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
 
-  resources :post_images, only:[:new, :index, :show]
+  resources :post_images, only:[:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
